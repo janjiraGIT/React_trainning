f34563c458fefc98481911f49dbccb3611d49f49
Janjira Eriksson <janjira.eriksson@gmail.com>
2021-03-11 21:25:01 +0100

    Add todo app

--- react-lesson/src/App.js
+++ react-lesson/src/App.js
@@ -1,32 +1,26 @@
-import React from "react"
+import React from 'react';
+
+import todosData from "./componentsTodo/todoData"
+import TodoItem from './componentsTodo/TodoItem';
+import TodoApp from "./componentsTodo/TodoApp"
+import "./App.css";
 
 class App extends React.Component {
 
   constructor() {
     super()
     this.state = {
-      isLoggedIn: true
+      hello: "Hello!"
     }
   }
-
-
   render() {
-
-    let displayText
-    if (this.state.isLoggedIn) {
-      displayText = "yes"
-    } else {
-      displayText = "no"
-    }
-
-
     return (
-      <div>
-        <h1>You are currently logged : {displayText}</h1>
+      <div className="app">
+        <h1>{this.state.hello}</h1>
+        <TodoApp />
       </div>
     )
   }
-
 }
 
 export default App
---
+++ react-lesson/src/componentsTodo/todoData.js
@@ -0,0 +1,34 @@
+const todosData = [
+    {
+        id:"1",
+        text: "Do home work",
+        completed: true
+    },
+    {
+        id:"2",
+        text: "Hair cut",
+        completed: false
+    },
+    {
+        id:"3",
+        text: "Swimming Class",
+        completed: false
+    },
+    {
+        id:"4",
+        text: "Clean your own room",
+        completed: true
+    },
+    {
+        id:"5",
+        text: "Practis Thai language",
+        completed: false
+    },
+    {
+        id:"6",
+        text: "Practis foot ball",
+        completed: true
+    },
+]
+
+export default todosData
