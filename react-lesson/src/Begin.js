f11a698038d6da4c6098a7d96b48552e2894873a
Janjira Eriksson <janjira.eriksson@gmail.com>
2021-04-18 17:59:10 +0200

    Add start to work with forms

---
+++ react-lesson/src/Begin.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react"
+
+// https://reactjs.org/docs/forms.html
+
+class Begin extends Component {
+    constructor() {
+        super()
+        this.state ={}
+    }
+
+    render() {
+        return(
+            <div>
+                Code here
+            </div>
+        )
+    }
+}
+
+export default Begin
