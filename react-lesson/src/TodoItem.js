af34dd901812f1dadf8e5541f59758ccc8b2025d
Janjira Eriksson <janjira.eriksson@gmail.com>
2021-04-18 12:13:54 +0200

    Add more style for TodoItem

--- react-lesson/src/TodoItem.js
+++ react-lesson/src/TodoItem.js
@@ -1,10 +1,17 @@
 import React from "react"
 
 function TodoItem(props) {
+
+    const completedStyle = {
+        fontSyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+
+    }
     return (
         <div className="todo-item">
             <input type="checkbox" checked={props.item.completed}/>
-            <p>{props.item.text}</p>
+            <p style={props.item.completed ? completedStyle:null}>{props.item.text}</p>
         </div>
     )
 }
